06acded56f46dc8f2a7646f04a351e0945bdc65e
Terence Honles <terence@honles.com>
2011-05-15 14:23:58 -0700

    added Python 2.4 support + reorganized library
     - removed minor issues which blocked Python 2.4 support
     - split library up into logical units
     - cleaned up setup
    
    --HG--
    rename : LICENSE => LICENSE.txt
    rename : README => README.txt

--- progressbar/__init__.py
+++ progressbar/__init__.py
@@ -42,14 +42,11 @@ automatically enable features like auto-resizing when the system supports it.
 
 from __future__ import division
 
-__author__ = 'Nilton Volpato'
-__author_email__ = 'first-name dot last-name @ gmail.com'
-__date__ = '2011-05-14'
-__version__ = '2.3'
-
-import datetime
 import math
-import sys, time, os
+import os
+import signal
+import sys
+import time
 
 try:
     from fcntl import ioctl
@@ -58,298 +55,17 @@ try:
 except ImportError:
     pass
 
-import signal
-
-# Python 3.x (and backports) use a modified iterator syntax
-# This will allow 2.x to behave with 3.x iterators
-if not hasattr(__builtins__, 'next'):
-    def next(iter):
-        try:
-            # Try new style iterators
-            return iter.__next__()
-        except AttributeError:
-            # Fallback in case of a "native" iterator
-            return iter.next()
-
-# Python 3.x int is practically synonymous for a long in 2.x so create an alias
-if not hasattr(__builtins__, 'long'): long = int
+from progressbar.compat import *
+from progressbar.widgets import *
 
 
-def noop_decorator(fn): return fn
+__author__ = 'Nilton Volpato'
+__author_email__ = 'first-name dot last-name @ gmail.com'
+__date__ = '2011-05-14'
+__version__ = '2.3'
 
-try: from abc import ABCMeta, abstractmethod
-except ImportError:
-   ABCMeta = None
-   abstractmethod = noop_decorator
-
-
-def format_updatable(updatable, pbar):
-    if hasattr(updatable, 'update'): return updatable.update(pbar)
-    else: return updatable
 
 class UnknownLength: pass
-
-
-class ProgressBarWidget(object):
-    '''The base class for all widgets
-
-    The ProgressBar will call the widget's update value when the widget should
-    be updated. The widget's size may change between calls, but the widget may
-    display incorrectly if the size changes drastically and repeatedly.
-
-    The boolean TIME_SENSITIVE informs the ProgressBar that it should be
-    updated more often because it is time sensitive.
-    '''
-    if ABCMeta is not None: __metaclass__ = ABCMeta
-
-    TIME_SENSITIVE = False
-    __slots__ = ()
-
-    @abstractmethod
-    def update(self, pbar):
-        '''Updates the widget.
-
-        pbar - a reference to the calling ProgressBar
-        '''
-
-
-class ProgressBarWidgetHFill(ProgressBarWidget):
-    '''The base class for all variable width widgets.
-
-    This widget is much like the \\hfill command in TeX, it will expand to
-    fill the line. You can use more than one in the same line, and they will
-    all have the same width, and together will fill the line.
-    '''
-
-    @abstractmethod
-    def update(self, pbar, width):
-        '''Updates the widget providing the total width the widget must fill.
-
-        pbar - a reference to the calling ProgressBar
-        width - The total width the widget must fill
-        '''
-
-
-class Timer(ProgressBarWidget):
-    'Widget which displays the elapsed seconds.'
-
-    __slots__ = ('format',)
-    TIME_SENSITIVE = True
-
-    def __init__(self, format='Elapsed Time: %s'):
-        self.format = format
-
-    @staticmethod
-    def format_time(seconds):
-        'Formats time as the string "HH:MM:SS".'
-
-        return str(datetime.timedelta(seconds=int(seconds)))
-
-
-    def update(self, pbar):
-        'Updates the widget to show the elapsed time.'
-
-        return self.format % self.format_time(pbar.seconds_elapsed)
-
-
-class ETA(Timer):
-    'Widget which attempts to estimate the time of arrival.'
-
-    TIME_SENSITIVE = True
-
-    def update(self, pbar):
-        'Updates the widget to show the ETA or total time when finished.'
-
-        if pbar.currval == 0:
-            return 'ETA:  --:--:--'
-        elif pbar.finished:
-            return 'Time: %s' % self.format_time(pbar.seconds_elapsed)
-        else:
-            elapsed = pbar.seconds_elapsed
-            eta = elapsed * pbar.maxval / pbar.currval - elapsed
-            return 'ETA:  %s' % self.format_time(eta)
-
-
-class FileTransferSpeed(ProgressBarWidget):
-    'Widget for showing the transfer speed (useful for file transfers).'
-
-    format = '%6.2f %s%s/s'
-    prefixes = ' kMGTPEZY'
-    __slots__ = ('unit', 'format')
-
-    def __init__(self, unit='B'):
-        self.unit = unit
-
-    def update(self, pbar):
-        'Updates the widget with the current SI prefixed speed.'
-
-        if pbar.seconds_elapsed < 2e-6 or pbar.currval < 2e-6: # =~ 0
-            scaled = power = 0
-        else:
-            speed = pbar.currval / pbar.seconds_elapsed
-            power = int(math.log(speed, 1000))
-            scaled = speed / 1000.**power
-
-        return self.format % (scaled, self.prefixes[power], self.unit)
-
-
-class AnimatedMarker(ProgressBarWidget):
-    '''An animated marker for the progress bar which defaults to appear as if
-    it were rotating.
-    '''
-
-    __slots__ = ('markers', 'curmark')
-
-    def __init__(self, markers='|/-\\'):
-        self.markers = markers
-        self.curmark = -1
-
-    def update(self, pbar):
-        '''Updates the widget to show the next marker or the first marker when
-        finished'''
-
-        if pbar.finished: return self.markers[0]
-
-        self.curmark = (self.curmark + 1) % len(self.markers)
-        return self.markers[self.curmark]
-
-# Alias for backwards compatibility
-RotatingMarker = AnimatedMarker
-
-
-class Counter(ProgressBarWidget):
-    'Displays the current count'
-
-    __slots__ = ('format',)
-
-    def __init__(self, format='%d'):
-        self.format = format
-
-    def update(self, pbar):
-        return self.format % pbar.currval
-
-
-class Percentage(ProgressBarWidget):
-    'Displays the current percentage as a number with a percent sign.'
-
-    def update(self, pbar):
-        return '%3d%%' % pbar.percentage()
-
-
-class FormatLabel(Timer):
-    'Displays a formatted label'
-
-    mapping = {
-        'elapsed': ('seconds_elapsed', Timer.format_time),
-        'finished': ('finished', None),
-        'last_update': ('last_update_time', None),
-        'max': ('maxval', None),
-        'seconds': ('seconds_elapsed', None),
-        'start': ('start_time', None),
-        'value': ('currval', None)
-    }
-
-    __slots__ = ('format',)
-    def __init__(self, format): self.format = format
-
-    def update(self, pbar):
-        context = {}
-        for name, (key, transform) in self.mapping.items():
-            try:
-                value = getattr(pbar, key)
-                context[name] = value if transform is None else transform(value)
-            except: pass
-
-        return self.format % context
-
-
-class SimpleProgress(ProgressBarWidget):
-    'Returns progress as a count of the total (e.g.: "5 of 47")'
-
-    __slots__ = ('sep',)
-
-    def __init__(self, sep=' of '):
-        self.sep = sep
-
-    def update(self, pbar):
-        return '%d%s%d' % (pbar.currval, self.sep, pbar.maxval)
-
-
-class Bar(ProgressBarWidgetHFill):
-    'A progress bar which stretches to fill the line.'
-
-    __slots__ = ('marker', 'left', 'right', 'fill', 'fill_left')
-
-    def __init__(self, marker='#', left='|', right='|', fill=' ',
-                 fill_left=True):
-        '''Creates a customizable progress bar.
-
-        marker - string or updatable object to use as a marker
-        left - string or updatable object to use as a left border
-        right - string or updatable object to use as a right border
-        fill - character to use for the empty part of the progress bar
-        fill_left - whether to fill from the left or the right
-        '''
-        self.marker = marker
-        self.left = left
-        self.right = right
-        self.fill = fill
-        self.fill_left = fill_left
-
-    def update(self, pbar, width):
-        'Updates the progress bar and its subcomponents'
-
-        left, marker, right = (format_updatable(i, pbar) for i in
-                               (self.left, self.marker, self.right))
-
-        width -= len(left) + len(right)
-        # Marker must *always* have length of 1
-        marker *= int(pbar.currval / pbar.maxval * width)
-
-        if self.fill_left:
-            return '%s%s%s' % (left, marker.ljust(width, self.fill), right)
-        else:
-            return '%s%s%s' % (left, marker.rjust(width, self.fill), right)
-
-
-class ReverseBar(Bar):
-    def __init__(self, marker='#', left='|', right='|', fill=' ',
-                 fill_left=False):
-        '''Creates a customizable progress bar.
-
-        marker - string or updatable object to use as a marker
-        left - string or updatable object to use as a left border
-        right - string or updatable object to use as a right border
-        fill - character to use for the empty part of the progress bar
-        fill_left - whether to fill from the left or the right
-        '''
-        self.marker = marker
-        self.left = left
-        self.right = right
-        self.fill = fill
-        self.fill_left = fill_left
-
-
-class BouncingBar(Bar):
-    def update(self, pbar, width):
-        'Updates the progress bar and its subcomponents'
-
-        left, marker, right = (format_updatable(i, pbar) for i in
-                               (self.left, self.marker, self.right))
-
-        width -= len(left) + len(right)
-
-        if pbar.finished: return '%s%s%s' % (left, width * marker, right)
-
-        position = int(pbar.currval % (width * 2 - 1))
-        if position > width: position = width * 2 - position
-        lpad = self.fill * (position - 1)
-        rpad = self.fill * (width - len(marker) - len(lpad))
-
-        # Swap if we want to bounce the other way
-        if not self.fill_left: rpad, lpad = lpad, rpad
-
-        return '%s%s%s%s%s' % (left, lpad, marker, rpad, right)
 
 
 class ProgressBar(object):
@@ -500,7 +216,7 @@ class ProgressBar(object):
         width = self.term_width
 
         for index, widget in enumerate(self.widgets):
-            if isinstance(widget, ProgressBarWidgetHFill):
+            if isinstance(widget, WidgetHFill):
                 result.append(widget)
                 expanding.insert(0, index)
             else:
@@ -545,10 +261,10 @@ class ProgressBar(object):
                                     for w in self.widgets)
 
 
-    def update(self, value):
+    def update(self, value=None):
         'Updates the ProgressBar to a new value.'
 
-        if value is not None:
+        if value is not None and value is not UnknownLength:
             if (self.maxval is not UnknownLength
                 and not 0 <= value <= self.maxval):
 
@@ -601,7 +317,7 @@ class ProgressBar(object):
         'Puts the ProgressBar bar in the finished state.'
 
         self.finished = True
-        self.update(self.maxval if self.maxval is not UnknownLength else None)
+        self.update(self.maxval)
         self.fd.write('\n')
         if self.signal_set:
             signal.signal(signal.SIGWINCH, signal.SIG_DFL)
--- setup.py
+++ setup.py
@@ -1,33 +1,55 @@
 #!/usr/bin/python
 
 import os
-from distutils.core import setup
+from setuptools import setup, find_packages
 import progressbar
 
-if (not os.path.exists('README') or
-    os.stat('progressbar.py').st_mtime > os.stat('README').st_mtime):
-    open('README','w').write(progressbar.__doc__)
+# TODO: I don't believe this should be in here. This should be done on package
+#       creation only
+try:
+    readme = 'README.txt'
+    info = 'progressbar/__init__.py'
+
+    if (not os.path.exists(readme) or
+        os.stat(info).st_mtime > os.stat(readme).st_mtime):
+
+        open(readme,'w').write(progressbar.__doc__)
+except: pass
 
 setup(
-    name = 'progressbar',
-    version = progressbar.__version__,
-    description = progressbar.__doc__.splitlines()[0],
-    long_description = progressbar.__doc__,
-    maintainer = progressbar.__author__,
-    maintainer_email = progressbar.__author_email__,
-    url = 'http://code.google.com/p/python-progressbar',
-    py_modules = [ 'progressbar' ],
-    classifiers = [
-    'Development Status :: 5 - Production/Stable',
-    'Environment :: Console',
-    'Intended Audience :: Developers',
-    'Intended Audience :: System Administrators',
-    'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
-    'License :: OSI Approved :: BSD License',
-    'Operating System :: OS Independent',
-    'Programming Language :: Python',
-    'Topic :: Software Development :: Libraries',
-    'Topic :: Software Development :: User Interfaces',
-    'Topic :: Terminals',
+    name='progressbar',
+    version=progressbar.__version__,
+    packages=find_packages(),
+
+    description=progressbar.__doc__.split('\n')[0],
+    long_description=progressbar.__doc__,
+
+    author=progressbar.__author__,
+    maintainer=progressbar.__author__,
+    author_email=progressbar.__author_email__,
+    maintainer_email=progressbar.__author_email__,
+
+    url='http://code.google.com/p/python-progressbar',
+    license='LICENSE.txt',
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: '
+            'GNU Library or Lesser General Public License (LGPL)',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.4',
+        'Programming Language :: Python :: 2.5',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Software Development :: User Interfaces',
+        'Topic :: Terminals'
     ],
 )
