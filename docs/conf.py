7cb4bba0befa43c8afab67fae10ceea17fd1c6b7
WoLpH <Rick@Fawo.nl>
2013-09-23 14:40:44 +0200

    updated docs

--- docs/conf.py
+++ docs/conf.py
@@ -30,8 +30,13 @@ import progressbar
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = [
-    'sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
-    'sphinx.ext.coverage', 'sphinx.ext.mathjax', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode']
+    'sphinx.ext.autodoc',
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage',
+    'sphinx.ext.viewcode',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -48,7 +53,7 @@ master_doc = 'index'
 # General information about the project.
 project = u'Progress Bar'
 project_slug = ''.join(project.capitalize().split())
-copyright = '%s, %s' % (
+copyright = u'%s,  <a href="http://wol.ph/">%s</a>' % (
     datetime.date.today().year,
     progressbar.__author__,
 )
