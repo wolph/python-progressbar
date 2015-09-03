e5c6986ebad526d9b8f79712244435ff465c4031
Rick van Hattem <Wolph@wol.ph>
2015-09-03 17:14:58 +0200

    fixed really stupid bug with sub-second timedeltas

--- progressbar/utils.py
+++ progressbar/utils.py
@@ -1,4 +1,23 @@
 
 def timedelta_to_seconds(delta):
-        return (delta.microseconds + (delta.seconds + delta.days * 24 * 3600) *
-                10 ** 6 / 10 ** 6)
+    '''Convert a timedelta to seconds with the microseconds as fraction
+
+    >>> from datetime import timedelta
+    >>> timedelta_to_seconds(timedelta(days=1))
+    86400
+    >>> timedelta_to_seconds(timedelta(seconds=1))
+    1
+    >>> timedelta_to_seconds(timedelta(seconds=1, microseconds=1))
+    1.000001
+    >>> timedelta_to_seconds(timedelta(microseconds=1))
+    1e-06
+    '''
+    # Only convert to float if needed
+    if delta.microseconds:
+        total = delta.microseconds * 1e-6
+    else:
+        total = 0
+    total += delta.seconds
+    total += delta.days * 60 * 60 * 24
+    return total
+
