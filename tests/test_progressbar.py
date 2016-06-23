5ab15c8b3750506146d1d31b4b56524590cc9173
Rick van Hattem <Wolph@wol.ph>
2016-06-23 22:27:23 +0200

    fixed output redirection tests

--- tests/test_progressbar.py
+++ tests/test_progressbar.py
@@ -9,6 +9,7 @@ def test_examples_nullbar(monkeypatch):
     # Patch progressbar to use null bar instead of regular progress bar
     import progressbar
     monkeypatch.setattr(progressbar, 'ProgressBar', progressbar.NullBar)
-    from examples import examples
-    for example in examples:
+    import examples
+    examples.non_interactive_sleep_factor = 10000
+    for example in examples.examples:
         example()
