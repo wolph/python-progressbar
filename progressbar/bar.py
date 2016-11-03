225cb1c719300e778359d2c8ef839272643d9140
Rick van Hattem <Wolph@wol.ph>
2016-11-03 08:30:36 +0100

    made sure to flush after writing to stream

--- progressbar/bar.py
+++ progressbar/bar.py
@@ -43,9 +43,13 @@ class DefaultFdMixin(ProgressBarMixinBase):
         ProgressBarMixinBase.__init__(self, **kwargs)
 
     def update(self, *args, **kwargs):
+        updates = self.updates
         ProgressBarMixinBase.update(self, *args, **kwargs)
         self.fd.write('\r' + self._format_line())
-        self.fd.flush()
+
+        # Only flush if something was actually written
+        if updates != self.updates:
+            self.fd.flush()
 
     def finish(self, *args, **kwargs):  # pragma: no cover
         ProgressBarMixinBase.finish(self, *args, **kwargs)
