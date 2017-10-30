54ed2eff0641f382bc7123dd601e0af15cfaa287
Rick van Hattem <Wolph@wol.ph>
2017-10-30 03:21:36 +0100

    made max_value error ignorable to fix #142

--- tests/test_large_values.py
+++ tests/test_large_values.py
@@ -7,3 +7,10 @@ def test_large_max_value():
         for i in range(10):
             bar.update(i)
             time.sleep(0.1)
+
+
+def test_value_beyond_max_value():
+    with progressbar.ProgressBar(max_value=10, max_error=False) as bar:
+        for i in range(20):
+            bar.update(i)
+            time.sleep(0.01)
