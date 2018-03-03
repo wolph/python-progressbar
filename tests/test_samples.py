9214fe09da8c31dca5990b4a596155597e6e7717
Rick van Hattem <Wolph@wol.ph>
2018-03-04 00:11:37 +0100

    extended samples tests

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -5,41 +5,68 @@ from progressbar import widgets
 
 
 def test_numeric_samples():
-    samples = widgets.SamplesMixin(samples=10)
-    bar = progressbar.ProgressBar(widgets=[samples])
+    samples = 5
+    samples_widget = widgets.SamplesMixin(samples=samples)
+    bar = progressbar.ProgressBar(widgets=[samples_widget])
 
     # Force updates in all cases
-    samples.INTERVAL = - timedelta(1)
-
-    bar.update(0)
-    assert samples(bar, None)[1] == [0, 0, 0]
-    bar.update(1)
-    assert samples(bar, None)[1] == [0, 0, 0, 1, 1]
-    bar.update(2)
-    assert samples(bar, None)[1] == [0, 0, 0, 1, 1, 2, 2]
-
-    assert samples(bar, None, delta=True)[1] == 2
-
-
-def test_timedelta_samples():
-    samples = widgets.SamplesMixin(samples=timedelta(seconds=5))
-    bar = progressbar.ProgressBar(widgets=[samples])
-
-    # Force updates in all cases
-    samples.INTERVAL = - timedelta(1)
+    samples_widget.INTERVAL = timedelta(0)
 
     start = datetime(2000, 1, 1)
 
     bar.value = 1
-    bar.last_update_time = start + timedelta(seconds=1)
-    assert samples(bar, None, True) == (None, None)
+    bar.last_update_time = start + timedelta(seconds=bar.value)
+    assert samples_widget(bar, None, True) == (None, None)
 
-    bar.value = 2
-    bar.last_update_time = start + timedelta(seconds=2)
-    assert samples(bar, None, True) == (timedelta(0, 1), 1)
+    for i in range(2, 6):
+        bar.value = i
+        bar.last_update_time = start + timedelta(seconds=i)
+        assert samples_widget(bar, None, True) == (timedelta(0, i - 1), i - 1)
 
-    bar.value = 3
-    bar.last_update_time = start + timedelta(seconds=3)
-    assert samples(bar, None, True) == (timedelta(0, 2), 2)
+    bar.value = 8
+    bar.last_update_time = start + timedelta(seconds=bar.value)
+    assert samples_widget(bar, None, True) == (timedelta(0, 6), 6)
 
-    assert samples(bar, None)[1] == [1, 2, 3, 3]
+    bar.value = 10
+    bar.last_update_time = start + timedelta(seconds=bar.value)
+    assert samples_widget(bar, None, True) == (timedelta(0, 7), 7)
+
+    bar.value = 20
+    bar.last_update_time = start + timedelta(seconds=bar.value)
+    assert samples_widget(bar, None, True) == (timedelta(0, 16), 16)
+
+    assert samples_widget(bar, None)[1] == [4, 5, 8, 10, 20]
+
+
+def test_timedelta_samples():
+    samples = timedelta(seconds=5)
+    samples_widget = widgets.SamplesMixin(samples=samples)
+    bar = progressbar.ProgressBar(widgets=[samples_widget])
+
+    # Force updates in all cases
+    samples_widget.INTERVAL = timedelta(0)
+
+    start = datetime(2000, 1, 1)
+
+    bar.value = 1
+    bar.last_update_time = start + timedelta(seconds=bar.value)
+    assert samples_widget(bar, None, True) == (None, None)
+
+    for i in range(2, 6):
+        bar.value = i
+        bar.last_update_time = start + timedelta(seconds=i)
+        assert samples_widget(bar, None, True) == (timedelta(0, i - 1), i - 1)
+
+    bar.value = 8
+    bar.last_update_time = start + timedelta(seconds=bar.value)
+    assert samples_widget(bar, None, True) == (timedelta(0, 5), 5)
+
+    bar.value = 10
+    bar.last_update_time = start + timedelta(seconds=bar.value)
+    assert samples_widget(bar, None, True) == (timedelta(0, 5), 5)
+
+    bar.value = 20
+    bar.last_update_time = start + timedelta(seconds=bar.value)
+    assert samples_widget(bar, None, True) == (timedelta(0, 10), 10)
+
+    assert samples_widget(bar, None)[1] == [10, 20]
