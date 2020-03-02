d9f443263e7606c94ae8a1561050aa86d1efaeae
Rick van Hattem <Wolph@wol.ph>
2020-03-02 16:47:27 +0100

    changing to unicode literals to fix ascii issues

--- examples.py
+++ examples.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import absolute_import
+from __future__ import division
 from __future__ import print_function
+from __future__ import unicode_literals
+from __future__ import with_statement
 
 import functools
 import random
