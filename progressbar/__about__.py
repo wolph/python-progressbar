7188aa39a062b0ff4b2bab24d7673668c07453d5
Rick van Hattem <Wolph@wol.ph>
2020-05-02 16:03:08 +0200

    Incrementing version to v3.51.3

--- progressbar/__about__.py
+++ progressbar/__about__.py
@@ -19,7 +19,7 @@ A Python Progressbar library to provide visual (yet text based) progress to
 long running operations.
 '''.strip().split())
 __email__ = 'wolph@wol.ph'
-__version__ = '3.51.2'
+__version__ = '3.51.3'
 __license__ = 'BSD'
 __copyright__ = 'Copyright 2015 Rick van Hattem (Wolph)'
 __url__ = 'https://github.com/WoLpH/python-progressbar'
